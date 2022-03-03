fe214d25234ea7425c769eb11544fb32c23d0ae4
07naoki07 <naokiti.0205.22@gmail.com>
2022-03-03 17:57:18 +0000

    4task 1st

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,28 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: {minimum: 2, maximum: 20 }
   validates :introduction,  length: {maximum: 50}
   
+    # フォローをした、されたの関係
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   
-has_one_attached :profile_image
+  # 一覧画面で使う
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+    
+    # フォローしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+  
+  has_one_attached :profile_image
   
   def get_profile_image(width, height)
   unless profile_image.attached?
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-    
-  
- 
   root to: "homes#top"
   devise_for :users
    resources :users, only: [:show, :edit, :update, :index]
@@ -10,5 +7,10 @@ Rails.application.routes.draw do
    resources :book_comments, only: [:create, :destroy]
   end
   get "/home/about" => "homes#about", as: "about"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+   resource :relationships, only: [:create, :destroy]
+   get 'followings' => 'relationships#followings', as: 'followings'
+   get 'followers' => 'relationships#followers', as: 'followers'
+  end
+   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
