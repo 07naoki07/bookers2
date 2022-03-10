d8b0bb82a93cc826f88dc20df7f60a0f50998272
07naoki07 <naokiti.0205.22@gmail.com>
2022-03-10 10:48:37 +0000

    6task finish

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,20 +3,14 @@ class BookCommentsController < ApplicationController
     @book = Book.find(params[:book_id])
     @comment = current_user.book_comments.new(book_comment_params)
     @comment.book_id = @book.id
-    if @comment.save
-     flash.now[:notice] = 'コメントを投稿しました'
-     format.html { redirect_back(fallback_location: root_path) } # 前のページに遷移
-     format.js  # create.js.erbが呼び出される
-    else
-      format.html { redirect_back(fallback_location: root_path) } # 前のページに遷移
-    end
+    @comment.save
+    flash.now[:notice] = 'コメントを投稿しました'
  end
  
  def destroy
   BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
   flash.now[:alert] = '投稿を削除しました'
   @book = Book.find(params[:book_id])  
-  render :show  #render先にjsファイルを指定
  end
 
   private
